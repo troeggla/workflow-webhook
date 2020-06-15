ffd786490e8add8ae1117e5c69850d04c0f0d8ed
Thomas Röggla <tom@lichtfragmente.com>
2020-06-15 23:00:22 +0200

    removed lines which are commented out

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,24 +17,21 @@ DATA_JSON="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"commi
 if [ -n "$data" ]; then
     COMPACT_JSON=$(echo -n "$data" | jq -c '')
     WEBHOOK_DATA="{$DATA_JSON,\"data\":$COMPACT_JSON}"
-    # WEBHOOK_DATA=$(jq -c . $GITHUB_EVENT_PATH)
 else
     WEBHOOK_DATA="{$DATA_JSON}"
 fi
 
 WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
-# WEBHOOK_SIGNATURE=$(cat "$GITHUB_EVENT_PATH" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
 
 WEBHOOK_ENDPOINT=$webhook_url
 if [ -n "$webhook_auth" ]; then
     WEBHOOK_ENDPOINT="-u $webhook_auth $webhook_url"
 fi
 
-curl -X POST \ 
-    -H "content-type: application/json" \
+curl -X POST \
+    -H "Content-Type: application/json" \
     -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
-    -H "x-hub-signature: sha1=$WEBHOOK_SIGNATURE" \
-    -H "x-gitHub-delivery: $GITHUB_RUN_NUMBER" \
-    -H "x-github-event: $GITHUB_EVENT_NAME" \
+    -H "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
+    -H "X-Github-Delivery: $GITHUB_RUN_NUMBER" \
+    -H "X-Github-Event: $GITHUB_EVENT_NAME" \
     --data "$WEBHOOK_DATA" $WEBHOOK_ENDPOINT
-    # -D - $WEBHOOK_ENDPOINT --data-urlencode @"$GITHUB_EVENT_PATH"
